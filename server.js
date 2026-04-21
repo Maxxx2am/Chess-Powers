4a79d4c91c80ff0539966b19323557ba056de1b8
Maxxx2am <eurohex@gmail.com>
2026-04-21 09:10:50 -0400

    Added socket.io for multiplayer

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    // When one player moves, send that move to the other player
+    socket.on('move', (moveData) => {
+        socket.broadcast.emit('move', moveData);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
